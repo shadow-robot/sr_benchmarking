ac38f435b87ed75d9ce41727aff2e65eef1b445e
Beatriz Leon <beatriz@shadowrobot.com>
2018-11-12 23:16:27 +0000

    Fixing lint

--- sr_moveit_planner_benchmarking/test/test_import_export_queries.py
+++ sr_moveit_planner_benchmarking/test/test_import_export_queries.py
@@ -8,6 +8,7 @@ from unittest import TestCase
 
 PKG = "sr_moveit_planner_benchmarking"
 
+
 class TestImportExportQueries(TestCase):
     def setUp(self):
         self.test_repo_path = rospkg.RosPack().get_path('sr_moveit_planner_benchmarking') + "/test/"
@@ -15,13 +16,17 @@ class TestImportExportQueries(TestCase):
         self.queries_to_import = self.test_repo_path + "test_queries_to_import.queries"
         self.queries_generated_after_export = self.tmp_path_for_generated_files + "test_scene.queries"
         rospy.sleep(25)
+
     def test_export_queries(self):
-        self.assertTrue(os.path.isfile(self.queries_generated_after_export), "Queries file was not generated %s" %self.queries_generated_after_export)
+        self.assertTrue(os.path.isfile(self.queries_generated_after_export),
+                        "Queries file was not generated %s" % self.queries_generated_after_export)
         with open(self.queries_to_import, 'r') as queries_imported:
             with open(self.queries_generated_after_export, 'r') as queries_exported:
                 difference = set(queries_imported).difference(queries_exported)
-                self.assertEqual(set([]), difference, "difference: %s" %difference)
+                self.assertEqual(set([]), difference, "difference: %s" % difference)
+
 
 if __name__ == '__main__':
     import rostest
+
     rostest.rosrun(PKG, 'test_import_export_queriess', TestImportExportQueries)
